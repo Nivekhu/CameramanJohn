ae1e2bf18255e7e5bea4c0926b17262b4eef31c3
Kevin Hu <ContactKevinHu@gmail.com>
2018-10-10 13:21:56 -0400

    removed my old makefile, got video working

--- makefile
+++
@@ -1,8 +0,0 @@
-CC = g++
-CFLAGS = -02 -g -c
-OpenCV = `pkg-config --cflags --libs opencv`
-
-.C.o:
-	$(CC) $(CFLAGS) $< -o $@
-all:
-	$(CC) main.c -o main $(OpenCV)
--- simpletest_raspicam_cv.cpp
+++ simpletest_raspicam_cv.cpp
@@ -10,19 +10,25 @@ int main ( int argc,char **argv ) {
 		time_t timer_begin,timer_end;
 		raspicam::RaspiCam_Cv Camera;
 		cv::Mat image;
-		int nCount=10;
+		int nCount=10000;
 		//set camera params
 		Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
 		//Open camera
 		cout<<"Opening Camera..."<<endl;
 		if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
+		
+		//Start Display
+		namedWindow("Window", WINDOW_AUTOSIZE);	
+		
 		//Start capture
 		cout<<"Capturing "<<nCount<<" frames ...."<<endl;
 		time ( &timer_begin );
 		for ( int i=0; i<nCount; i++ ) {
 				Camera.grab();
-				Camera.retrieve ( image);
-				if ( i%5==0 )  cout<<"\r captured "<<i<<" images"<<std::flush;
+				Camera.retrieve (image);
+				imshow("Window", image);
+				cout<<"\r captured "<<i<<" images"<<std::flush;
+				waitKey(1);
 		}
 		cout<<"Stop camera..."<<endl;
 		Camera.release();
@@ -31,12 +37,5 @@ int main ( int argc,char **argv ) {
 		time ( &timer_end ); /* get current time; same as: timer = time(NULL)  */
 		double secondsElapsed = difftime ( timer_end,timer_begin );
 		cout<< secondsElapsed<<" seconds for "<< nCount<<"  frames : FPS = "<<  ( float ) ( ( float ) ( nCount ) /secondsElapsed ) <<endl;
-		//display the image
-		namedWindow("DisplayWindow", WINDOW_AUTOSIZE);
-		imshow("DisplayWindow", image);
-		cout<<"displaying image"<<endl;
-		
-		//waits on a key press
-		waitKey(0);
 		cout<<"closing program"<<endl;
 }
