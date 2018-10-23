5e87cf4affa32ff907f558867e7c528c434a1252
Kevin Hu <ContactKevinHu@gmail.com>
2018-10-23 13:14:48 -0400

    changed the input so that the file paths are hardcoded

--- simpletest_raspicam_cv.cpp
+++ simpletest_raspicam_cv.cpp
@@ -1,23 +1,41 @@
-#include <sstream>
-#include <fstream>
-#include <iostream>
-
 #include <raspicam/raspicam_cv.h>
 
-#include <opencv2/objdetect.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/face.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
+/*
+ * Copyright (c) 2011. Philipp Wagner <bytefish[at]gmx[dot]de>.
+ * Released to public domain under terms of the BSD Simplified license.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the organization nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ *   See <http://www.opensource.org/licenses/bsd-license>
+ */
+
+#include "opencv2/core.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/objdetect.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace cv;
 using namespace cv::face;
-using namespace std; 
+using namespace std;
 
 static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
 		std::ifstream file(filename.c_str(), ifstream::in);
 		if (!file) {
-				string error_message = "No valid input file was given,please check the given filename.";
+				string error_message = "No valid input file was given, please check the given filename.";
 				CV_Error(CV_StsBadArg, error_message);
 		}
 		string line, path, classlabel;
@@ -32,9 +50,10 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
 		}
 }
 
-int main ( int argc,char **argv ) {
+int main(int argc, const char *argv[]) {
 		// Check for valid command line arguments, print usage
 		// if no arguments were given.
+		/*
 		if (argc != 4) {
 				cout << "usage: " << argv[0] << " </path/to/haar_cascade> </path/to/csv.ext> </path/to/device id>" << endl;
 				cout << "\t </path/to/haar_cascade> -- Path to the Haar Cascade for face detection." << endl;
@@ -42,10 +61,11 @@ int main ( int argc,char **argv ) {
 				cout << "\t <device id> -- The webcam device id to grab frames from." << endl;
 				exit(1);
 		}
+		*/
 		// Get the path to your CSV:
-		string fn_haar = string(argv[1]);
-		string fn_csv = string(argv[2]);
-		int deviceId = atoi(argv[3]);
+		string fn_haar = "/home/pi/opencv-3.4.1/data/haarcascades/haarcascade_frontalface_default.xml";
+		string fn_csv = "/home/pi/Desktop/CameramanJohn/faces.txt";
+		int deviceId = 0;
 		// These vectors hold the images and corresponding labels:
 		vector<Mat> images;
 		vector<int> labels;
@@ -57,77 +77,64 @@ int main ( int argc,char **argv ) {
 				// nothing more we can do
 				exit(1);
 		}
-
-		//Face recognition variables
+		// Get the height from the first image. We'll need this
+		// later in code to reshape the images to their original
+		// size AND we need to reshape incoming faces to this size:
+		int im_width = images[0].cols;
+		int im_height = images[0].rows;
+		// Create a FaceRecognizer and train it on the given images:
 		Ptr<FaceRecognizer> model = FisherFaceRecognizer::create();
+		// Training the face classifier
 		model->train(images, labels);
 		CascadeClassifier haar_cascade;
 		haar_cascade.load(fn_haar);
-		int im_width = images[0].cols;
-		int im_height = images[0].rows;
-
-		//Camera/image
-		raspicam::RaspiCam_Cv Camera;
-		cv::Mat image;
-		//set camera params
-		Camera.set( CV_CAP_PROP_FORMAT, CV_8UC3 );
-		//Open camera
-		cout<<"Opening Camera..."<<endl;
-		if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
-
-		//Start Display
-		namedWindow("Window", WINDOW_AUTOSIZE);	
-
-		//Start capture
-		cout<<"Capturing..."<<endl;
-		while (true) {
-				Camera.grab();
-				Camera.retrieve (image);
-
-				//Cloning the image and converting it to grayscale
-				Mat copyImg = image.clone();
-				Mat grayImg;
-				cvtColor(copyImg, grayImg, CV_BGR2GRAY);
-
+		// Get a handle to the Video device:
+		VideoCapture cap(deviceId);
+		// Check if we can use this device at all:
+		if(!cap.isOpened()) {
+				cerr << "Capture Device ID " << deviceId << "cannot be opened." << endl;
+				return -1;
+		}
+		// Holds the current frame from the Video device:
+		Mat frame;
+		for(;;) {
+				cap >> frame;
+				// Clone the current frame:
+				Mat original = frame.clone();
+				// Convert the current frame to grayscale:
+				Mat gray;
+				cvtColor(original, gray, CV_BGR2GRAY);
+				// Find the faces in the frame:
 				vector< Rect_<int> > faces;
-				haar_cascade.detectMultiScale(grayImg, faces);
-				// At this point you have the position of the faces in
-				// faces. Now we'll get the faces, make a prediction and
-				// annotate it in the video. Cool or what?
+				haar_cascade.detectMultiScale(gray, faces);
+				// At this point you have the position of the faces
 				for(int i = 0; i < faces.size(); i++) {
 						// Process face by face:
 						Rect face_i = faces[i];
-						// Crop the face from the image. So simple with OpenCV C++:
-						Mat face = grayImg(face_i);
-						
-						//Resizing the image
+						// Crop the face from the image.
+
+						Mat face = gray(face_i);
 						Mat face_resized;
 						cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
-						
 						// Now perform the prediction, see how easy that is:
 						int prediction = model->predict(face_resized);
 						
-						// And finally write all we've found out to the original image!
-						// First of all draw a green rectangle around the detected face:
-						rectangle(copyImg, face_i, CV_RGB(0, 255,0), 1);
-						// Create the text we will annotate the box with:
-						string box_text = format("Prediction = %d", prediction);
-						// Calculate the position for annotated text (make sure we don't
-						// put illegal values in there):
-						int pos_x = std::max(face_i.tl().x - 10, 0);
-						int pos_y = std::max(face_i.tl().y - 10, 0);
-						// And now put it into the image:
-						putText(copyImg, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0,255,0), 2.0);
-				}
+						//Displaying face prediction
+						rectangle(original, face_i, CV_RGB(255, 0,0), 1);
+						//Calculate position of target
+						int pos_x = face_i.tl().x;
+						int pos_y = face_i.tl().y;
 
-				imshow("Window", image);
+						// And now put it into the image:
+						circle(original, Point(pos_x*2, pos_y*2), 1.0, CV_RGB(255,0,0), 2.0);
+				}
+				// Show the result:
+				imshow("face_recognizer", original);
+				// And display it:
 				char key = (char) waitKey(20);
+				// Exit this loop on escape:
 				if(key == 27)
 						break;
 		}
-		cout<<"Stop camera..."<<endl;
-		Camera.release();
-		cout<<"Closing program..."<<endl;
-		
+		return 0;
 }
-
