1154616e281e402e0de762d3374adbfd3a326b9c
Kevin Hu <ContactKevinHu@gmail.com>
2018-11-06 15:09:48 -0500

    added some code for following only one face. needs improvement

--- simpletest_raspicam_cv.cpp
+++ simpletest_raspicam_cv.cpp
@@ -70,7 +70,7 @@ int main(int argc, const char *argv[]) {
 		string fn_haar = "/home/pi/opencv-3.4.1/data/haarcascades/haarcascade_frontalface_default.xml";
 		string fn_csv = "/home/pi/Desktop/CameramanJohn/faces.txt";
 		int deviceId = 0;
-		
+
 		// This sets up the GPIO Pins using wiring Pi
 		wiringPiSetup();
 
@@ -83,18 +83,18 @@ int main(int argc, const char *argv[]) {
 
 		// Hardcoded half-step sequence for a stepper motor
 		int seq[8][4] = {{1,0,0,0},
-						 {1,1,0,0},
-						 {0,1,0,0},
-						 {0,1,1,0},
-						 {0,0,1,0},
-						 {0,0,1,1},
-						 {0,0,0,1},
-						 {1,0,0,1}};
+				{1,1,0,0},
+				{0,1,0,0},
+				{0,1,1,0},
+				{0,0,1,0},
+				{0,0,1,1},
+				{0,0,0,1},
+				{1,0,0,1}};
 
 		// These vectors hold the images and corresponding labels:
 		vector<Mat> images;
 		vector<int> labels;
-		
+
 		// Read in the data (fails if no valid input filename is given, but you'll get an error message):
 		try {
 				read_csv(fn_csv, images, labels);
@@ -103,32 +103,32 @@ int main(int argc, const char *argv[]) {
 				// nothing more we can do
 				exit(1);
 		}
-		
+
 		// Get the height from the first image. We'll need this
 		// later in code to reshape the images to their original
 		// size AND we need to reshape incoming faces to this size:
 		int im_width = images[0].cols;
 		int im_height = images[0].rows;
-		
+
 		// Create a FaceRecognizer and train it on the given images:
 		Ptr<FaceRecognizer> model = FisherFaceRecognizer::create();
-		
+
 		// Training the face classifier
 		cout << "Trainging..." << endl;
 		model->train(images, labels);
 		cout << "Done training" << endl;
 		CascadeClassifier haar_cascade;
 		haar_cascade.load(fn_haar);
-		
+
 		// Get a handle to the Video device:
 		VideoCapture cap(deviceId);
-		
+
 		// Check if we can use this device at all:
 		if(!cap.isOpened()) {
 				cerr << "Capture Device ID " << deviceId << "cannot be opened." << endl;
 				return -1;
 		}
-		
+
 		// Holds the current frame from the Video device:
 		Mat frame;
 		for(;;) {
@@ -142,42 +142,47 @@ int main(int argc, const char *argv[]) {
 				vector< Rect_<int> > faces;
 				haar_cascade.detectMultiScale(gray, faces);
 				//Creates a confidence variable to compare to
-				double global_conf = 0.0
+				double global_conf = std::numeric_limits<float>::infinity();;
 				// At this point you have the position of the faces
 				for(int i = 0; i < faces.size(); i++) {
 						// Process face by face:
 						Rect face_i = faces[i];
-						
+
 						// Crop the face from the image.
 						Mat face = gray(face_i);
 						Mat face_resized;
 						cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
-						
+
 						//Checking the confidence of the face
 						int label = -1;
 						double confidence = 0.0;
 						model->predict(face_resized, label, confidence);
-						
-						//Displaying face prediction
-						rectangle(original, face_i, CV_RGB(255, 0,0), 1);
 
-						//Calculate position of target
-						int pos_x = face_i.tl().x;
-						int pos_y = face_i.tl().y;
-						
-						// And now put it into the image:
-						circle(original, Point(pos_x+face_i.width/2, pos_y+face_i.height/2), 1.0, CV_RGB(255,0,0), 2.0);
-						
-						//Calculate the corner of the rectangle
-						int text_pos_x = std::max(face_i.tl().x - 10, 0);
-						int text_pos_y = std::max(face_i.tl().y - 10, 0);
-						
-						//Display coords
-						string boxtext = format("x=%d y=%d confidence=%f", pos_x, pos_y, confidence);
-						putText(original, boxtext, Point(text_pos_x, text_pos_y),FONT_HERSHEY_PLAIN, 1.0, CV_RGB(255,0,0), 2.0);
+						if(confidence <= global_conf + 100)
+						{
+								global_conf = confidence;
+								//Displaying face prediction
+								rectangle(original, face_i, CV_RGB(255, 0,0), 1);
 
-						//Rotates the camera
-					
+								//Calculate position of target
+								int pos_x = face_i.tl().x;
+								int pos_y = face_i.tl().y;
+
+								// And now put it into the image:
+								circle(original, Point(pos_x+face_i.width/2, pos_y+face_i.height/2), 1.0, CV_RGB(255,0,0), 2.0);
+
+								//Calculate the corner of the rectangle
+								int text_pos_x = std::max(face_i.tl().x - 10, 0);
+								int text_pos_y = std::max(face_i.tl().y - 10, 0);
+
+								//Display coords
+								string boxtext = format("x=%d y=%d confidence=%f", pos_x, pos_y, confidence);
+								putText(original, boxtext, Point(text_pos_x, text_pos_y),FONT_HERSHEY_PLAIN, 1.0, CV_RGB(255,0,0), 2.0);
+
+								//Rotates the camera
+
+
+						}
 				}
 				// Show the result:
 				imshow("face_recognizer", original);
